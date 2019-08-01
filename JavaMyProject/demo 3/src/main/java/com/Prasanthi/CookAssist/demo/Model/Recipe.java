9cf70c3ff21f1e16697b370b6fdc1112cf6de81a
prasanthipenmadu <prasanthipl@gmail.com>
2019-08-01 09:52:47 -0400

    Table created

--- JavaMyProject/demo 3/src/main/java/com/Prasanthi/CookAssist/demo/Model/Recipe.java
+++ JavaMyProject/demo 3/src/main/java/com/Prasanthi/CookAssist/demo/Model/Recipe.java
@@ -3,19 +3,14 @@ package com.Prasanthi.CookAssist.demo.Model;
 import lombok.Data;
 
 import javax.lang.model.element.Name;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
-
 public class Recipe {
     @Id
-    @GeneratedValue(generator = "system-uuid")
-
-    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     @Column(name = "name")
     private String name;
     @Column(name = "recipeStepNumber")
--- JavaMyProject/demo 3/src/main/resources/data.sql
+++ JavaMyProject/demo 3/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into Recipe (id,name,recipeStepNumber,recipeStepProcedure,cookTime,
+insert into Recipe (id,name,recipe_step_number,recipe_step_procedure,cook_time,
 ingredients)values ( '1','MangoShake','1' ,'Gather the Ingredients','10','Mango pulp,Milk,Sugar,whipped cream' );
